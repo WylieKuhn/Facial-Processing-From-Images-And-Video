a0282ed60c679ba5a74495f8c035fcb392569f92
Wylie Kuhn <wyliekuhnaero@gmail.com>
2023-12-04 02:52:35 -0500

    Fixed incorrect  variable name

--- video_frame_extractor.py
+++ video_frame_extractor.py
@@ -6,7 +6,7 @@ output_folder = "Video Frames"
 
 # Create the output directory if it doesn't exist
 if not os.path.exists(output_folder):
-os.makedirs(output_folder, exist_ok=True)
+    os.makedirs(output_folder, exist_ok=True)
 
 # Extract every 30th frame assuming 30fps footage, this can be altered to extract any n-frames for performance and time
 n_frames = 29  
@@ -18,7 +18,7 @@ while True:
         break
 
     if frame_number % (n_frames + 1) == 0:
-        filename = os.path.join(output_dir, "frame_" + str(frame_number) + ".jpg")
+        filename = os.path.join(output_folder, "frame_" + str(frame_number) + ".jpg")
         cv2.imwrite(filename, frame)
 
     frame_number += 1
