e95df2c480df5f360cd6ae6db3a8dbcb0cc6d494
Wylie Kuhn <86081235+WylieKuhn@users.noreply.github.com>
2023-12-04 18:32:30 -0500

    Finder now checks every frame in a folder of saved frames
    Finder now checks every frame in a folder of saved frames against a folder of faces and outputs a csv file containing the relative paths of the matching face, and the master frame.

--- finder.py
+++ finder.py
@@ -1,11 +1,41 @@
 from deepface import DeepFace
+import os
+import pandas as pd 
 
-# Set the path to the image of the face you want to search for
-image = r"images/face.jpg"
 
 # Set the folder with the extracted video frames you want to search
-search_folder = "Video Frames 2"
+db = "faces"
+search_folder = "output_frames"
 
-dfs = DeepFace.find(img_path = image, db_path = search_folder, enforce_detection=False, detector_backend = "retinaface")
 
-print(dfs)
+master_results_dataframe = pd.DataFrame(columns=["identity", "source_x", "source_y", "source_w", "source_h", "VGG-Face_cosine"])
+image_count = 0
+
+for filename in os.listdir(search_folder):
+    file_path = os.path.join(search_folder, filename)
+     
+    try:   
+        img = DeepFace.extract_faces(file_path, detector_backend="retinaface", enforce_detection=False)
+        
+        if img is not None:
+            
+            dfs = DeepFace.find(img_path = file_path, db_path = db, enforce_detection=False, detector_backend = "retinaface")
+            
+            for df in dfs:
+                df["master_frame"] = file_path
+                dfs_dataframe = pd.DataFrame(df)
+                master_results_dataframe = pd.concat([master_results_dataframe, dfs_dataframe], ignore_index=True)
+        else:
+            print("File Not Found")
+            
+    except Exception as e:
+        print(f"Error processing image {file_path}: {str(e)}")
+    
+    image_count += 1
+    
+    print(f"Processed image {image_count} of 111")
+        
+csv_filename = "output_results.csv"
+master_results_dataframe.to_csv(csv_filename, index=False)
+
+print(master_results_dataframe)
