b3843526d1e023e04a91add4528d2b69593b6aeb
Wylie Kuhn <wyliekuhnaero@gmail.com>
2023-12-06 03:24:57 -0500

    Updated CLI and finder functiona
    Find command now shows how many frames you have process of total extracted frames and the completion percentage.

--- finder.py
+++ finder.py
@@ -4,38 +4,46 @@ import pandas as pd
 
 
 # Set the folder with the extracted video frames you want to search
-db = "faces"
-search_folder = "output_frames"
 
 
-master_results_dataframe = pd.DataFrame(columns=["identity", "source_x", "source_y", "source_w", "source_h", "VGG-Face_cosine"])
-image_count = 0
+def face_finder(check_directory, face_directory):
 
-for filename in os.listdir(search_folder):
-    file_path = os.path.join(search_folder, filename)
-     
-    try:   
-        img = DeepFace.extract_faces(file_path, detector_backend="retinaface", enforce_detection=False)
+    master_results_dataframe = pd.DataFrame(columns=["identity", "source_x", "source_y", "source_w", "source_h", "VGG-Face_cosine"])
+    image_count = 0
+    num_files_in_directory = 0
+
+    for filename in os.listdir(check_directory):
+        num_files_in_directory += 1
+
+    for filename in os.listdir(check_directory):
+        file_path = os.path.join(check_directory, filename)
         
-        if img is not None:
+        try:   
+            img = DeepFace.extract_faces(file_path, detector_backend="retinaface", enforce_detection=False)
             
-            dfs = DeepFace.find(img_path = file_path, db_path = db, enforce_detection=False, detector_backend = "retinaface")
-            
-            for df in dfs:
-                df["master_frame"] = file_path
-                dfs_dataframe = pd.DataFrame(df)
-                master_results_dataframe = pd.concat([master_results_dataframe, dfs_dataframe], ignore_index=True)
-        else:
-            print("File Not Found")
-            
-    except Exception as e:
-        print(f"Error processing image {file_path}: {str(e)}")
-    
-    image_count += 1
-    
-    print(f"Processed image {image_count} of 111")
+            if img is not None:
+                
+                dfs = DeepFace.find(img_path = file_path, db_path = face_directory, enforce_detection=False, detector_backend = "retinaface")
+                
+                for df in dfs:
+                    df["master_frame"] = file_path
+                    dfs_dataframe = pd.DataFrame(df)
+                    master_results_dataframe = pd.concat([master_results_dataframe, dfs_dataframe], ignore_index=True)
+            else:
+                print("File Not Found")
+                
+        except Exception as e:
+            print(f"Error processing image {file_path}: {str(e)}")
         
-csv_filename = "output_results.csv"
-master_results_dataframe.to_csv(csv_filename, index=False)
+        image_count += 1
+        
+        print(f"Processed image {image_count} of {num_files_in_directory}, {round((image_count/num_files_in_directory)*100,2)}% finished")
+            
+    csv_filename = "output_results.csv"
+    master_results_dataframe.to_csv(csv_filename, index=False)
 
-print(master_results_dataframe)
+    print(master_results_dataframe)
+
+#db = "faces"
+#search_folder = "output_frames"
+#face_finder(search_folder, db)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pyfiglet import Figlet
 from video_frame_extractor_v2 import extract_frames
+from finder import face_finder
 
 f = Figlet(font='slant')
 print(f.renderText('FaceFinder'))
@@ -8,7 +9,8 @@ while True:
     print("""
     Select an option:
         extract: Extract every n frames of a video and save them in an output folder you specify
-        search-frames: search a folder of frames against a folder of faces and get a csv file of all possible matches
+        find: search a folder of frames against a folder of faces and get a csv file of all possible matches
+        exit: close the program
     """)
     
     selection = input("Make A Selection: ")
@@ -19,6 +21,13 @@ while True:
         output_folder_name = input("Enter The name of the folder where you wish to store the saved frames: ")
         frame_skip = int(input("Extract every n-frames (enter a whole number): "))
         extract_frames(input_video, output_folder_name, frame_skip)
+    elif selection.lower() == "find":
+        frame_folder = input("Enter the input path of the folder of frames you wish to check: ")
+        face_folder = input("Enter the input path of the folder of faces you wish search for in the frames: ")
+        face_finder(frame_folder, face_folder)
+    elif selection.lower() == "exit":
+        print("Closing Program")
+        break
     else:
         print("Invalid Selection")
     
